2b58c273cb3492f28d5c3fcf9ee612997783eca5
Pablo <pablo.deyzaguirre@globalincubator.com>
2019-03-27 14:06:41 +0100

    Fetch singular records - displaying the userHeader

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,14 @@
 import jsonPlaceholder from '../APIs/jsonPlaceholder';
 
 export const fetchPosts = () => async dispatch => {
-    const response = await jsonPlaceholder.get('/posts');
+  const response = await jsonPlaceholder.get('/posts');
         
-    dispatch({ type: 'FETCH_POSTS', payload: response.data })
-    };
+  dispatch({ type: 'FETCH_POSTS', payload: response.data })
+  };
+
+//                  the ID of the user we want to fetch
+export const fetchUser = (id) => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+        
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+  };
